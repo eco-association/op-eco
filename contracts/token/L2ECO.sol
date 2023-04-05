2817ab9ccf1de186069766dd51dd1a865b061973
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2023-04-05 12:09:02 -0400

    formating directory structure and pulling interfaces out to inherit from op

--- contracts/token/L2ECO.sol
+++ contracts/token/L2ECO.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.19;
 
+import {L2StandardERC20} from "@eth-optimism/contracts/standards/L2StandardERC20.sol";
 import {IL2ECO} from "../interfaces/token/IL2ECO.sol";
 
-abstract contract L2ECO is IL2ECO {
-
+/**
+ * @title L2ECO
+ */
+contract L2ECO is L2StandardERC20, IL2ECO {
+    constructor(address _l2Bridge, address _l1Token)
+        L2StandardERC20(_l2Bridge, _l1Token, "Optimism ECO", "OP-ECO")
+    {}
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -72,7 +72,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.9',
+        version: '0.8.19',
         settings: {
           optimizer: { enabled: true, runs: 10_000 },
         },
