56dc276ef23d80a6cf37c57b4537d2e2b49720bd
Albert Brown <albertbrown@Bobingy.local>
2023-04-05 13:10:18 -0700

    more role management framework

--- contracts/token/L2ECO.sol
+++ contracts/token/L2ECO.sol
@@ -17,7 +17,6 @@ contract L2ECO is ERC20Pausable, DelegatePermit {
   mapping(address => bool) public minters;
   mapping(address => bool) public burners;
   mapping(address => bool) public rebasers;
-  mapping(address => bool) public upgraders;
 
   // to be used to record the transfer amounts after _beforeTokenTransfer
   // these values are the base (unchanging) values the currency is stored in
@@ -32,13 +31,20 @@ contract L2ECO is ERC20Pausable, DelegatePermit {
     */
   event NewInflationMultiplier(uint256 inflationMultiplier);
 
+  // when admin becomes mutable, we put it in the initialize function
   constructor(
-      address _l2Bridge,
-      address _l1Token,
-      address admin,
-      address _initialPauser
-  ) ERC20Pausable("Optimism ECO", "OP-ECO", admin, _initialPauser) {
+      address admin
+  ) ERC20Pausable("Optimism ECO", "OP-ECO", admin, address(0)) {}
+
+  function initialize(
+    address _l2Bridge,
+    address _initialPauser
+  ) public {
     _linearInflationMultiplier = INITIAL_INFLATION_MULTIPLIER;
+    minters[_l2Bridge] = true;
+    burners[_l2Bridge] = true;
+    rebasers[_l2Bridge] = true;
+    pauser = _initialPauser;
   }
 
   modifier onlyMinterRole() {
@@ -48,6 +54,16 @@ contract L2ECO is ERC20Pausable, DelegatePermit {
 
   modifier onlyBurnerRole() {
     require(burners[msg.sender], "not authorized to burn");
+    _;
+  }
+
+  modifier onlyRebaserRole() {
+    require(rebasers[msg.sender], "not authorized to rebase");
+    _;
+  }
+
+  modifier onlyRoleAdmin() {
+    require(msg.sender == roleAdmin, "not authorized to edit roles");
     _;
   }
 
@@ -84,4 +100,29 @@ contract L2ECO is ERC20Pausable, DelegatePermit {
   function burn(address _from, uint256 _value) external onlyBurnerRole {
     _burn(_from, _value);
   }
+
+  function rebase(uint256 _newLinearInflationMultiplier) external onlyRebaserRole {
+    _rebase(_newLinearInflationMultiplier);
+  }
+
+  function _rebase(uint256 _newLinearInflationMultiplier) internal {
+    _linearInflationMultiplier = _newLinearInflationMultiplier;
+  }
+
+  function updateMinters(address _key, bool _value) public onlyRoleAdmin {
+    minters[_key] = _value;
+  }
+
+  function updateBurners(address _key, bool _value) public onlyRoleAdmin {
+    burners[_key] = _value;
+  }
+
+  function updateRebasers(address _key, bool _value) public onlyRoleAdmin {
+    rebasers[_key] = _value;
+  }
+
+  // requires change to inherited contract
+  // function updateRoleAdmin(address _newAdmin) public onlyRoleAdmin {
+  //   roleAdmin = _newAdmin;
+  // }
 }
