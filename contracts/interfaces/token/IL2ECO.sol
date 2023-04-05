7ed74edff661f7071b5d057da0254689c9b39bbd
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2023-04-05 11:32:02 -0400

    adding placeholder l2 eco interface and abstract contract

---
+++ contracts/interfaces/token/IL2ECO.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {IL2StandardERC20} from "@eth-optimism/contracts/standards/IL2StandardERC20.sol";
+
+interface IL2ECO is IL2StandardERC20 {
+    
+}
---
+++ contracts/token/L2ECO.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {IL2ECO} from "../interfaces/token/IL2ECO.sol";
+
+abstract contract L2ECO is IL2ECO {
+
+}
