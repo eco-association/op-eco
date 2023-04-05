bd0314c527db9ce9d183232938b5dbace24e19f6
Albert Brown <albertbrown@Bobingy.local>
2023-04-05 10:17:50 -0700

    removed unnecessary compatability function

--- contracts/L2ECO.sol
+++ contracts/L2ECO.sol
@@ -65,19 +65,6 @@ contract L2ECO is ERC20Pausable, DelegatePermit {
       return gonsAmount;
   }
 
-  // maybe we need this function for compatibility?
-  function getPastLinearInflation(uint256 blockNumber)
-      public
-      view
-      returns (uint256)
-  {
-      require(
-          blockNumber <= block.number,
-          "InflationCheckpoints: cannot check future block"
-      );
-      return _linearInflationMultiplier;
-  }
-
   /** Access function to determine the token balance held by some address.
     */
   function balanceOf(address _owner) public view override returns (uint256) {
