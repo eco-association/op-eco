9e36650baca586f736ae431b369ae489f985fd33
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2023-04-17 15:57:27 -0400

    updating test

--- test/utils/errors.ts
+++ test/utils/errors.ts
@@ -8,6 +8,9 @@ export const ERROR_STRINGS = {
     INVALID_X_DOMAIN_MSG_SENDER:
       'OVM_XCHAIN: wrong sender of cross-domain message',
   },
+  L1ECOBridge: {
+    UNAUTHORIZED_UPGRADER: 'L1ECOBridge: caller not authorized to upgrade L2 contracts.'
+  },
   L2ECOBridge: {
     INVALID_L2ECO_ADDRESS: 'L2ECOBridge: invalid L2ECO token address',
     INVALID_L1_ADDRESS: 'L2ECOBridge: invalid L1 token address',
@@ -18,6 +21,7 @@ export const ERROR_STRINGS = {
     UNAUTHORIZED_BURNER: 'L2ECO: not authorized to burn',
     UNAUTHORIZED_REBASER: 'L2ECO: not authorized to rebase',
     UNAUTHORIZED_TOKEN_ROLE_ADMIN: 'L2ECO: not authorized to edit roles',
+    
   },
   UPGRADES: {
     ALREADY_INITIALIZED: 'Initializable: contract is already initialized',
