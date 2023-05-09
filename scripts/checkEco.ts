e23d370860f617b0417848a4b1dc9d839ec1f321
Albert Brown <albertbrown@Bobingy.local>
2023-05-09 15:12:01 -0700

    make this script functional

--- scripts/checkEco.ts
+++ scripts/checkEco.ts
@@ -1,11 +1,13 @@
-import { ethers } from 'hardhat'
+import hre from 'hardhat'
 import { L2ECOBridge } from '../typechain-types'
 
+const l2BridgeProxyAddress = '0x7a01E277B8fDb8CDB2A2258508514716359f44A0'
+const L2_NETWORK = 'goerliOptimism'
+
 async function main() {
-  const L2ECOBridgeContract = await ethers.getContractFactory('L2ECOBridge')
-  const bridge = (L2ECOBridgeContract.attach(
-    process.env.L2_BRIDGE_ADDRESS || ''
-  )) as L2ECOBridge
+  hre.changeNetwork(L2_NETWORK)
+  const L2ECOBridgeContract = await hre.ethers.getContractFactory('L2ECOBridge')
+  const bridge = (L2ECOBridgeContract.attach(l2BridgeProxyAddress)) as L2ECOBridge
 
   console.log(`l1TokenBridge ${await bridge.l1TokenBridge()}`)
   console.log(`l2EcoToken ${await bridge.l2EcoToken()}`)
