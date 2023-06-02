be4433937a29fccc032d3373c172e89692828410
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2023-06-02 14:42:59 -0400

    fixing gap size

--- contracts/token/ERC20PermitUpgradeable.sol
+++ contracts/token/ERC20PermitUpgradeable.sol
@@ -108,5 +108,5 @@ abstract contract ERC20PermitUpgradeable is Initializable, ERC20Upgradeable, IER
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[49] private __gap;
+    uint256[50] private __gap;
 }
