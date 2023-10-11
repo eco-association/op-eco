bd4e6978bb8f7ab29ff5c55599cbaaded89d613f
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2023-10-11 14:44:58 -0400

    adding faucet changes

---
+++ arguments.js
@@ -0,0 +1,9 @@
+// arguments for the constructor of the EcoID contract when deployed to mainnet
+// used for etherscan verify of EcoID contract
+module.exports = [
+  "0xe7bc9b3a936f122f08aac3b1fac3c3ec29a78874", // eco
+  0,
+  0,
+  '0xDD1834f5116c4E2DCE952220dE5641cc775FdaaD',
+  ['0xDD1834f5116c4E2DCE952220dE5641cc775FdaaD']
+]
---
+++ scripts/faucetDeploy.ts
@@ -0,0 +1,21 @@
+import { ethers, upgrades } from "hardhat"
+import { Faucet } from '../typechain-types/contracts/token/Faucet'
+
+async function main() {
+  const ecoAddress = '0xe7bc9b3a936f122f08aac3b1fac3c3ec29a78874'
+  const faucetFactory = await ethers.getContractFactory('Faucet')
+
+  const faucet: Faucet = await faucetFactory.deploy(
+    ecoAddress,
+    0,
+    0,
+    '0xDD1834f5116c4E2DCE952220dE5641cc775FdaaD',
+    ['0xDD1834f5116c4E2DCE952220dE5641cc775FdaaD'])
+
+  console.log("Faucet deployed to:", faucet.address)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
