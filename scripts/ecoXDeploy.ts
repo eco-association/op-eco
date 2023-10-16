d4d9a0d7fd58e9fc58c2e0737a0f3ae9238a1b80
Albert Brown <albertbrown@Bobingy.local>
2023-10-16 15:18:37 -0700

    deploy script

---
+++ scripts/ecoXDeploy.ts
@@ -0,0 +1,30 @@
+import hre from 'hardhat'
+import {
+  deployProxy,
+  getProxyAdmin,
+  upgradeEcoXL2,
+} from '../test/utils/fixtures'
+import {
+  L1_ECOX_ADDRESS,
+  L2_NETWORK,
+  L2_OP_STANDARD_BRIDGE,
+  l2BridgeProxyAddress,
+} from './constants'
+
+async function main() {
+  hre.changeNetwork(L2_NETWORK)
+
+  const l2ECOxProxyAddress = await deployProxy()
+  console.log(`L2 ECOx proxy deployed to: ${l2ECOxProxyAddress}`)
+
+  const l2ProxyAdmin = await getProxyAdmin(true)
+  console.log(`Proxy Admin L2 deployed to: ${l2ProxyAdmin.address}`)
+
+  await upgradeEcoXL2(l2ECOxProxyAddress, L1_ECOX_ADDRESS, L2_OP_STANDARD_BRIDGE, l2BridgeProxyAddress)
+  console.log(`L2 ECOx initialized`)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
