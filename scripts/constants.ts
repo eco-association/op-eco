c5cd7edbca2992aa13227551b09b4d6f3a421f02
Albert Brown <albertbrown@Bobingy.local>
2023-10-18 15:44:32 -0700

    format

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,18 +1,18 @@
-//change these for different networks
-//GOERLI address
+// change these for different networks
+// GOERLI address
 const L1_OP_MESSANGER_ADDRESS = '0x5086d1eEF304eb5284A0f6720f79403b4e9bE294'
 const L2_OP_MESSANGER_ADDRESS = '0x4200000000000000000000000000000000000007'
 const L1_ECO_ADDRESS = '0x3E87d4d9E69163E7590f9b39a70853cf25e5ABE3'
 const L1_NETWORK = 'goerli'
 const L2_NETWORK = 'goerliOptimism'
 
-//goerli values
+// goerli values
 const l1BridgeProxyAddress = '0x7a01E277B8fDb8CDB2A2258508514716359f44A0'
 const l2BridgeProxyAddress = '0x7a01E277B8fDb8CDB2A2258508514716359f44A0'
 const l2EcoProxyAddress = '0x54bBECeA38ff36D32323f8A754683C1F5433A89f'
 const upgraderAddress = '' // bridge owns proxyAdmin now
 
-//mainnet values
+// mainnet values
 // const l1BridgeProxyAddress = '0xBa8be7D566c1C57aEbe0f9a077146619186ad062'
 // const l2BridgeProxyAddress = '0x8F854068Fd6E3a1c2ae175B33F703775Fe04CCf2'
 // const l2EcoProxyAddress = '0xe6Ac2D9e9FEb17020721127777eb78afCdf47f00'
