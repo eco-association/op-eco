b49e888b8a11a16b96a56dbddce2c5760522704a
Albert Brown <albertbrown@Bobingy.local>
2023-10-18 15:44:09 -0700

    adding in some extra data

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -6,11 +6,17 @@ const L1_ECO_ADDRESS = '0x3E87d4d9E69163E7590f9b39a70853cf25e5ABE3'
 const L1_NETWORK = 'goerli'
 const L2_NETWORK = 'goerliOptimism'
 
-//change these per deploys
-const l1BridgeProxyAddress = '0xBa8be7D566c1C57aEbe0f9a077146619186ad062'
-const l2BridgeProxyAddress = '0x8F854068Fd6E3a1c2ae175B33F703775Fe04CCf2'
-const l2EcoProxyAddress = '0xe6Ac2D9e9FEb17020721127777eb78afCdf47f00'
-const upgraderAddress = '0xafcE51A3bFEd22490343E25f66b10bD8677E22B9'
+//goerli values
+const l1BridgeProxyAddress = '0x7a01E277B8fDb8CDB2A2258508514716359f44A0'
+const l2BridgeProxyAddress = '0x7a01E277B8fDb8CDB2A2258508514716359f44A0'
+const l2EcoProxyAddress = '0x54bBECeA38ff36D32323f8A754683C1F5433A89f'
+const upgraderAddress = '' // bridge owns proxyAdmin now
+
+//mainnet values
+// const l1BridgeProxyAddress = '0xBa8be7D566c1C57aEbe0f9a077146619186ad062'
+// const l2BridgeProxyAddress = '0x8F854068Fd6E3a1c2ae175B33F703775Fe04CCf2'
+// const l2EcoProxyAddress = '0xe6Ac2D9e9FEb17020721127777eb78afCdf47f00'
+// const upgraderAddress = '0xafcE51A3bFEd22490343E25f66b10bD8677E22B9'
 
 export {
   L1_ECO_ADDRESS,
@@ -21,5 +27,5 @@ export {
   l1BridgeProxyAddress,
   l2BridgeProxyAddress,
   l2EcoProxyAddress,
-  upgraderAddress,//eco root policy
+  upgraderAddress,
 }
