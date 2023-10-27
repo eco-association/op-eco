f597e933b094bf2e69a6c193ba9c6596d8a3e2ef
Albert Brown <albert@eco.com>
2023-10-27 10:32:03 -0700

    Eco 1723 - upgrade pattern for ECOx (#48)
    * contract functionality
    
    * tests work
    created a messy pattern for making sure the constant address can be serviced in the test
    
    * format/lint
    
    ---------
    
    Co-authored-by: Albert Brown <albertbrown@Bobingy.local>

---
+++ contracts/test/poodleECOx.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "../token/L2ECOx.sol";
+
+contract PoodleL2ECOx is L2ECOx {
+
+    uint256 public constant NUM_POODLES = 1;
+
+    constructor() L2ECOx() {}
+}
